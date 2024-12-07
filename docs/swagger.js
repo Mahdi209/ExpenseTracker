dfa718c469dc2f35fff80cd38724fab848c1ae38
Mahdi209 <dr.arcadea@gmail.com>
2024-12-07 20:00:07 +0300

    edit swagger

--- docs/swagger.js
+++ docs/swagger.js
@@ -10,7 +10,8 @@ const options = {
         },
         servers: [
             {
-                url: `http://localhost:${process.env.PORT || 5000}`,
+                url: `https://expensetracker-b267.onrender.com/api/`,
+            }`,
                 description: 'Development server',
             },
         ],
